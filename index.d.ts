fb1e8ac434672b9607eac23e1ed8f09452a32665
Paweł Lidwin <lidwinpawel@gmail.com>
2023-05-21 09:56:54 +0200

    improv: enable strip on release profile

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+export function get_instance_version(): [
+  maj: number,
+  min: number,
+  patch: number
+];
+export function get_physical_versions(): {
+  title: string;
+  major: number;
+  minor: number;
+  patch: number;
+}[];
